58710c02cc3c867407be403b908f04cafb717f4b
Lukas Freyler <lukas.freyler@student.tugraz.at>
2021-04-14 12:43:20 +0200

    blub

--- Aufgabe/Dataset.py
+++ Aufgabe/Dataset.py
@@ -2,6 +2,16 @@
 class Dataset:
     def __init__(self, ):
         pass
-    def __str__():
+    
+    def __str__(self):
+        pass
+
+    def write_data_to_file(self):
+        pass
+
+    def get_feature_values(self):
+        pass
+
+    def get_target_values(self):
         pass
 # %%
