43bb3cd770dd2b3a5b77df5679ee6a87f14429a6
Lukas Freyler <lukas.freyler@student.tugraz.at>
2021-04-14 12:43:44 +0200

    ad

---
+++ Aufgabe/config_reader.py
@@ -0,0 +1,10 @@
+# %%
+
+class ConfigReader:
+
+    def __init__(self, config_file):
+        self.config_file = config_file
+
+    def read_json_config(self):
+        pass
+# %%
---
+++ Aufgabe/dataset_explorer.py
@@ -0,0 +1,2 @@
+class DatasetExplorer:
+    pass
